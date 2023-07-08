d12924061fed1f8c03587fe3bed2832d405c87f4
User <java.user.tester@gmail.com>
2023-07-08 12:08:48 +0300

    controller added

---
+++ src/main/java/edu/itstep/blockchain/UserController.java
@@ -0,0 +1,28 @@
+package edu.itstep.blockchain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import edu.itstep.blockchain.domain.User;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class UserController {
+	@Autowired
+	UserService userService;
+	@PostMapping(
+			  value = "/createUser", consumes = "application/json", produces = "application/json")
+			public User createUser(@RequestBody User user) {
+			    return userService.saveUpdatePerson(user);
+			}
+
+			@PostMapping(
+			  value = "/updateUser", consumes = "application/json", produces = "application/json")
+			public User updatePerson(@RequestBody User person, HttpServletResponse response) {
+			    response.setHeader("Location", ServletUriComponentsBuilder.fromCurrentContextPath()
+			      .path("/findPerson/" + person.getId()).toUriString());
+			    
+			    return userService.saveUpdatePerson(person);
+			}
+}
---
+++ src/main/java/edu/itstep/blockchain/UserService.java
@@ -0,0 +1,14 @@
+package edu.itstep.blockchain;
+
+import org.springframework.stereotype.Service;
+
+import edu.itstep.blockchain.domain.User;
+
+@Service
+public class UserService {
+   public  User  saveUpdatePerson(User user) {
+	   return user;
+   }
+   
+  
+}
